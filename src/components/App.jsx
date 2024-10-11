e9af937e0ca764fa524eef2ed6a84285a9103cc7
Julimm1 <163172417+Julimm1@users.noreply.github.com>
2024-10-11 23:16:14 +0300

    NotFound

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,17 @@
-import React from "react";
 import Header from "./Header/Header";
 import { Route, Routes } from "react-router-dom";
+import Home from "../pages/Home/Home";
+import About from "../pages/About/About";
+import NotFound from "../pages/NotFound/NotFound";
 
 const App = () => {
   return (
     <div>
       <Header />
       <Routes>
-        <Route path="/" element={<h1>Homepage</h1>}></Route>
-        <Route path="/about" element={<h1>About page</h1>}></Route>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="*" element={<NotFound />}></Route>
       </Routes>
     </div>
   );
